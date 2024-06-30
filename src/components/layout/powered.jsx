c6954aa9a6de4deae585af7972355e27d718c5b5
Abdul Malek Zihad <zihadm654@gmail.com>
2024-06-30 20:15:25 +0600

    responsiveness

--- src/components/layout/powered.jsx
+++ src/components/layout/powered.jsx
@@ -7,7 +7,7 @@ const Powered = () => {
         <h2 className="text-center text-3xl font-bold uppercase">
         Trusted by Leading Organisations 
         </h2>
-        <div className="mt-10 flex items-center justify-center space-x-16 max-md:space-x-4">
+        <div className="mt-10 flex items-center justify-center space-x-16 max-md:space-x-3">
           <img src="/image 39.png" alt="icons" className="object-cover aspect-auto max-md:h-11" />
           <img src="/image 38.png" alt="icons" className="object-cover max-md:h-11" />
           <img src="/reczm7ynHnFrnUpS2logowellx-logo (1).png" alt="icons" className="object-cover max-md:h-11" />
